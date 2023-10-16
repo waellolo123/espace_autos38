f3127a1649ef15874944a1c33f873cf65042d983
wael <wael.22@live.fr>
2023-10-16 14:33:48 +0100

    last cmmit

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+const humburger = document.querySelector('.hamburger');
+const closeBtn = document.querySelector('.close');
+const navList = document.querySelector('.nav-list');
+
+humburger.addEventListener('click', ()=>{
+  navList.classList.add('show-nav');
+  closeBtn.style.visibility = 'visible';
+  humburger.style.visibility = 'hidden';
+})
+closeBtn.addEventListener('click', ()=>{
+  navList.classList.remove('show-nav');
+  closeBtn.style.visibility = 'hidden';
+  humburger.style.visibility = 'visible';
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
